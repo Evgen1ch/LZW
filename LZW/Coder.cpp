d4577d75b3338cbf1ce1ac7994eeda29fcbffa26
Eugene <12345eug@gmail.com>
2021-02-24 14:36:00 +0200

    Coder class made static. Fields and Code method

--- LZW/Coder.cpp
+++ LZW/Coder.cpp
@@ -1,1 +1,49 @@
 #include "Coder.h"
+unsigned char Coder::mask = (1 << (BITS - 8)) - 1;
+unsigned char Coder::suffix = 0;
+unsigned int Coder::shift = _BITS - 8;
+
+void Coder::Code(const std::string& source, const std::string& target)
+{
+	std::ifstream input(source);
+	std::ofstream output(target);
+
+	if (!input || !output) {
+		return;
+	}
+
+	char a;
+	input >> a;
+	std::string w(1, a);
+	unsigned short code = a;
+
+	while (!input.eof()) {
+
+		input >> a;
+		std::string wa = w + a;
+
+		auto it = dictionary.find(wa);
+		if (it != dictionary.end()) {
+			w = wa;
+			code = it->second;
+		}
+		else {
+			dictionary.emplace(wa, dictionary.size());
+			w = a;
+			//write code to output
+			code = a;
+		}
+	}
+	//write code to output, but need to prevent double write of one code
+}
+
+void Coder::WriteCode(unsigned short code)
+{
+}
+
+void Coder::Reset()
+{
+	mask = (1 << (BITS - 8)) - 1;
+	suffix = 0;
+	shift = BITS - 8;
+}
--- LZW/Coder.h
+++ LZW/Coder.h
@@ -1,6 +1,21 @@
 #pragma once
-class Coder
+#include "global.h"
+#include <map>
+#include <string>
+#include <fstream>
+static class Coder
 {
+private: 
+	static const int _BITS = BITS;
+	static std::map<std::string, unsigned short> dictionary;
 
+	static unsigned char mask;
+	static unsigned char suffix;
+	static unsigned int shift;
+	
+public:
+	static void Code(const std::string& source, const std::string& target);
+	static void WriteCode(unsigned short code);
+	static void Reset();
 };
 
